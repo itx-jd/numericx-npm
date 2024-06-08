411877b6432854df74635deafadb5430b3211f7e
he-is-talha <talhayousaf4420@gmail.com>
2024-06-09 00:14:59 +0500

    Added Quote40 Option

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 
 const Popup = require('./models/Popup');
 const Quote = require('./models/Quote');
+const Quote40 = require('./models/Quote40');
 const excel = require('exceljs');
 const moment = require('moment');
 
@@ -104,6 +105,98 @@ app.delete('/quotes/:id', async (req, res) => {
 });
 
 
+// CRUD for FortyPercentQuotes
+app.get('/quotes40', async (req, res) => {
+  try {
+    const quotes = await Quote40.find();
+    res.json(quotes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.post('/quotes40', async (req, res) => {
+  const newQuote = new Quote40(req.body);
+    try {
+        const savedQuote = await newQuote.save();
+        res.status(201).json(savedQuote);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+// GET route to render quote40Records.ejs with all quotes
+app.get('/quote40Records', checkSession, async (req, res) => {
+  try {
+    const quotes = await Quote40.find();
+    res.render('quote40Records', { quotes });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+// POST request to download quotes40 as an Excel file
+app.post('/download_quotes40', async (req, res) => {
+  try {
+    const quotes = await Quote40.find();
+
+    // Create a new Excel workbook
+    const workbook = new excel.Workbook();
+    const worksheet = workbook.addWorksheet('Quotes40');
+
+    // Add headers
+    const headers = [
+      'Timestamp', 'Name', 'Email', 'Phone', 'Business Type', 'Dormant', 'Non-Trading', 'Free Company Formation',
+      'Annual Turnover', 'VAT Returns', 'Payroll', 'Number of Employees', 'Pension Scheme',
+      'Number of Employees Enrolled', 'Bookkeeping', 'Number of Transactions', 'Quote Price'
+    ];
+    worksheet.addRow(headers);
+
+    // Add data
+    quotes.forEach(quote => {
+      // Format timestamp
+      const formattedTimestamp = moment(quote.timestamp).format('YYYY-MM-DD HH:mm:ss');
+      
+      const row = [
+        formattedTimestamp,
+        quote.name,
+        quote.email,
+        quote.phone,
+        quote.businessType,
+        quote.dormant,
+        quote.nonTrading,
+        quote.freeCompanyFormation,
+        quote.annualTurnover,
+        quote.vatReturns,
+        quote.payroll,
+        quote.numberOfEmployees,
+        quote.pensionScheme,
+        quote.numberOfEmployeesEnrolled,
+        quote.bookkeeping,
+        quote.numberOfTransactions,
+        quote.quotePrice
+      ];
+      worksheet.addRow(row);
+    });
+
+    // Set response headers for Excel file download
+    res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    res.setHeader('Content-Disposition', 'attachment; filename=quotes40.xlsx');
+
+    // Send the workbook as a buffer
+    await workbook.xlsx.write(res);
+    res.end();
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error generating Excel file');
+  }
+});
+
+
 // GET route to render popupRecords.ejs with all popup
 app.get('/popupRecords', checkSession, async (req, res) => {
   try {
@@ -114,6 +207,7 @@ app.get('/popupRecords', checkSession, async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
+// POST request to download popups as an Excel file
 app.post('/download_popups', async (req, res) => {
   try {
     const popups = await Popup.find();
@@ -156,6 +250,7 @@ app.post('/download_popups', async (req, res) => {
     res.status(500).send('Error generating Excel file');
   }
 });
+
 
 
 // GET route to render quoteRecords.ejs with all quotes
---
+++ models/Quote40.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const Quote40Schema = new mongoose.Schema({
+  timestamp: { type: Date, default: Date.now },
+  name: String,
+  email: String,
+  phone: String,
+  businessType: String,
+  dormant: String,
+  nonTrading: String,
+  freeCompanyFormation: String,
+  annualTurnover: String,
+  vatReturns: String,
+  payroll: String,
+  numberOfEmployees: String,
+  pensionScheme: String,
+  numberOfEmployeesEnrolled: String,
+  bookkeeping: String,
+  numberOfTransactions: String,
+  quotePrice: String
+});
+
+module.exports = mongoose.model('Quote40', Quote40Schema);
